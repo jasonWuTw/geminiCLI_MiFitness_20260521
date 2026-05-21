315a428f4d8b70aa31b32bca46016031d0b2e029
Jason Wu <jason.wu.tw@gmail.com>
2026-05-21 14:06:02 +0800

    Initial commit of MiFitness data and source code

---
+++ sourceCode/data.js
@@ -0,0 +1,816 @@
+const dailyData = [
+  {
+    "date": "2026-01-01",
+    "steps": 13088,
+    "calories": 530.44,
+    "distance": 7.76,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-02",
+    "steps": 5386,
+    "calories": 220.0,
+    "distance": 3.19,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-03",
+    "steps": 16961,
+    "calories": 678.44,
+    "distance": 10.07,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-04",
+    "steps": 10544,
+    "calories": 421.76,
+    "distance": 6.24,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-05",
+    "steps": 5447,
+    "calories": 230.96,
+    "distance": 3.22,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-06",
+    "steps": 9495,
+    "calories": 385.24,
+    "distance": 5.61,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-07",
+    "steps": 6732,
+    "calories": 280.28,
+    "distance": 3.98,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-08",
+    "steps": 7470,
+    "calories": 307.56,
+    "distance": 4.41,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-09",
+    "steps": 8895,
+    "calories": 372.92,
+    "distance": 5.25,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-10",
+    "steps": 7628,
+    "calories": 305.12,
+    "distance": 4.52,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-11",
+    "steps": 9295,
+    "calories": 372.08,
+    "distance": 5.49,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-12",
+    "steps": 8203,
+    "calories": 339.64,
+    "distance": 4.86,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-13",
+    "steps": 8223,
+    "calories": 342.0,
+    "distance": 4.86,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-14",
+    "steps": 5510,
+    "calories": 222.64,
+    "distance": 3.27,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-15",
+    "steps": 6914,
+    "calories": 289.0,
+    "distance": 4.09,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-16",
+    "steps": 7840,
+    "calories": 343.92,
+    "distance": 4.64,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-17",
+    "steps": 8986,
+    "calories": 366.6,
+    "distance": 5.32,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-18",
+    "steps": 13054,
+    "calories": 522.16,
+    "distance": 7.73,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-19",
+    "steps": 6930,
+    "calories": 301.28,
+    "distance": 4.11,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-20",
+    "steps": 5420,
+    "calories": 221.4,
+    "distance": 3.21,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-21",
+    "steps": 4691,
+    "calories": 199.2,
+    "distance": 2.77,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-22",
+    "steps": 4562,
+    "calories": 194.92,
+    "distance": 2.7,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-23",
+    "steps": 10890,
+    "calories": 447.12,
+    "distance": 6.46,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-24",
+    "steps": 7926,
+    "calories": 317.04,
+    "distance": 4.68,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-25",
+    "steps": 10199,
+    "calories": 410.28,
+    "distance": 6.03,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-26",
+    "steps": 6359,
+    "calories": 259.48,
+    "distance": 3.77,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-27",
+    "steps": 7592,
+    "calories": 308.12,
+    "distance": 4.48,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-28",
+    "steps": 6028,
+    "calories": 247.44,
+    "distance": 3.58,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-29",
+    "steps": 5730,
+    "calories": 233.48,
+    "distance": 3.4,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-30",
+    "steps": 5434,
+    "calories": 217.36,
+    "distance": 3.2,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-01-31",
+    "steps": 12720,
+    "calories": 509.44,
+    "distance": 7.53,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-01",
+    "steps": 5066,
+    "calories": 202.64,
+    "distance": 3.0,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-02",
+    "steps": 7712,
+    "calories": 321.24,
+    "distance": 4.56,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-03",
+    "steps": 6543,
+    "calories": 266.92,
+    "distance": 3.86,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-04",
+    "steps": 6737,
+    "calories": 270.56,
+    "distance": 3.98,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-05",
+    "steps": 6227,
+    "calories": 254.38,
+    "distance": 3.7,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-06",
+    "steps": 6982,
+    "calories": 303.04,
+    "distance": 4.13,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-07",
+    "steps": 7611,
+    "calories": 304.44,
+    "distance": 4.51,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-08",
+    "steps": 9487,
+    "calories": 379.48,
+    "distance": 5.61,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-09",
+    "steps": 6884,
+    "calories": 282.36,
+    "distance": 4.08,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-10",
+    "steps": 5797,
+    "calories": 241.52,
+    "distance": 3.43,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-11",
+    "steps": 6239,
+    "calories": 260.16,
+    "distance": 3.69,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-12",
+    "steps": 17039,
+    "calories": 696.68,
+    "distance": 10.09,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-13",
+    "steps": 6485,
+    "calories": 276.16,
+    "distance": 3.84,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-02-14",
+    "steps": 2318,
+    "calories": 92.72,
+    "distance": 1.37,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-16",
+    "steps": 7998,
+    "calories": 319.92,
+    "distance": 4.75,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-17",
+    "steps": 6426,
+    "calories": 286.2,
+    "distance": 3.81,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-18",
+    "steps": 5304,
+    "calories": 212.16,
+    "distance": 3.14,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-19",
+    "steps": 6417,
+    "calories": 263.68,
+    "distance": 3.8,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-20",
+    "steps": 6311,
+    "calories": 261.36,
+    "distance": 3.73,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-21",
+    "steps": 6474,
+    "calories": 266.44,
+    "distance": 3.83,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-22",
+    "steps": 7441,
+    "calories": 317.12,
+    "distance": 4.4,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-23",
+    "steps": 6404,
+    "calories": 365.08,
+    "distance": 3.75,
+    "hr": 91,
+    "spo2": 97,
+    "stress": 21,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-24",
+    "steps": 11890,
+    "calories": 1198.4,
+    "distance": 6.93,
+    "hr": 97,
+    "spo2": 98,
+    "stress": 21,
+    "sleep": 5.05
+  },
+  {
+    "date": "2026-04-25",
+    "steps": 10707,
+    "calories": 780.12,
+    "distance": 5.99,
+    "hr": 97,
+    "spo2": 98,
+    "stress": 19,
+    "sleep": 5.47
+  },
+  {
+    "date": "2026-04-26",
+    "steps": 5811,
+    "calories": 447.32,
+    "distance": 3.26,
+    "hr": 96,
+    "spo2": 98,
+    "stress": 16,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-04-27",
+    "steps": 12844,
+    "calories": 919.4,
+    "distance": 7.46,
+    "hr": 109,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 5.93
+  },
+  {
+    "date": "2026-04-28",
+    "steps": 16513,
+    "calories": 988.0,
+    "distance": 9.47,
+    "hr": 104,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 2.48
+  },
+  {
+    "date": "2026-04-29",
+    "steps": 26886,
+    "calories": 1144.96,
+    "distance": 14.87,
+    "hr": 111,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 6.1
+  },
+  {
+    "date": "2026-04-30",
+    "steps": 13027,
+    "calories": 652.32,
+    "distance": 7.17,
+    "hr": 0,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.87
+  },
+  {
+    "date": "2026-05-01",
+    "steps": 15599,
+    "calories": 891.68,
+    "distance": 9.07,
+    "hr": 127,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 8.2
+  },
+  {
+    "date": "2026-05-02",
+    "steps": 1033,
+    "calories": 42.2,
+    "distance": 0.59,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-03",
+    "steps": 2641,
+    "calories": 110.84,
+    "distance": 1.55,
+    "hr": 0,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-04",
+    "steps": 21515,
+    "calories": 856.48,
+    "distance": 11.65,
+    "hr": 0,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 5.9
+  },
+  {
+    "date": "2026-05-05",
+    "steps": 10011,
+    "calories": 513.96,
+    "distance": 5.92,
+    "hr": 111,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-06",
+    "steps": 14470,
+    "calories": 897.92,
+    "distance": 8.13,
+    "hr": 101,
+    "spo2": 98,
+    "stress": 12,
+    "sleep": 4.17
+  },
+  {
+    "date": "2026-05-07",
+    "steps": 7851,
+    "calories": 367.84,
+    "distance": 4.61,
+    "hr": 0,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-08",
+    "steps": 23629,
+    "calories": 1695.48,
+    "distance": 19.94,
+    "hr": 121,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-09",
+    "steps": 6816,
+    "calories": 285.44,
+    "distance": 3.73,
+    "hr": 97,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-10",
+    "steps": 15763,
+    "calories": 907.4,
+    "distance": 8.73,
+    "hr": 104,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-11",
+    "steps": 11320,
+    "calories": 625.38,
+    "distance": 6.34,
+    "hr": 117,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-12",
+    "steps": 13325,
+    "calories": 954.36,
+    "distance": 7.6,
+    "hr": 120,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-13",
+    "steps": 4470,
+    "calories": 253.16,
+    "distance": 3.62,
+    "hr": 0,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-14",
+    "steps": 11265,
+    "calories": 749.24,
+    "distance": 6.51,
+    "hr": 0,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-15",
+    "steps": 14772,
+    "calories": 738.8,
+    "distance": 8.36,
+    "hr": 125,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-16",
+    "steps": 16074,
+    "calories": 647.6,
+    "distance": 8.85,
+    "hr": 0,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-17",
+    "steps": 10304,
+    "calories": 484.84,
+    "distance": 5.67,
+    "hr": 0,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-18",
+    "steps": 8309,
+    "calories": 358.88,
+    "distance": 4.57,
+    "hr": 0,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-19",
+    "steps": 9651,
+    "calories": 824.96,
+    "distance": 5.38,
+    "hr": 96,
+    "spo2": 97,
+    "stress": 0,
+    "sleep": 0.0
+  },
+  {
+    "date": "2026-05-20",
+    "steps": 16902,
+    "calories": 1262.24,
+    "distance": 9.56,
+    "hr": 141,
+    "spo2": 98,
+    "stress": 0,
+    "sleep": 4.7
+  },
+  {
+    "date": "2026-05-21",
+    "steps": 4624,
+    "calories": 341.4,
+    "distance": 2.76,
+    "hr": 105,
+    "spo2": 0,
+    "stress": 0,
+    "sleep": 0.0
+  }
+];
+const sportData = {
+  "walking": 28,
+  "outdoor_riding": 6
+};
